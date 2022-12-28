bf155098899667c48f9b9863ef0330054379c7ff
drew-chidi <chidi.andrew@gmail.com>
2022-12-28 13:35:25 +0100

    add hero for rooms page

---
+++ components/rooms/Hero.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { AiOutlineContacts } from "react-icons/ai";
+import Button from "../ui/Button";
+import Card from "../ui/Card";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={styles.heroContainer}>
+      <Card>
+        <div className={styles.welcomeBox}>
+          <p className={styles.welcomep1}>WELCOME TO</p>
+          <h1 className={styles.welcomeHeading}>ROYAL CREST</h1>
+          <p className={styles.welcomep2}>HOTELS</p>
+          <p className={styles.welcomep3}>
+            Book your stay and enjoy Luxury redefined at the most affordable
+            rates.
+            <br />
+            Enjoy luxury redefined at the most affordable rates
+          </p>
+          <Button title='BOOK NOW' iconName='book' />
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Hero;
